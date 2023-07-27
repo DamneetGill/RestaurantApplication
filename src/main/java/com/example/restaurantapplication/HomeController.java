a72782c06f2bc5fcfea24988627d182408e9d59a
DamneetGill <damneetsingh22@gmail.com>
2023-07-27 20:47:40 +0200

    Added login page and made a connection between controllers

--- src/main/java/com/example/restaurantapplication/HomeController.java
+++ src/main/java/com/example/restaurantapplication/HomeController.java
@@ -1,18 +1,41 @@
 package com.example.restaurantapplication;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.util.Duration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
+import java.io.IOException;
+
 
 public class HomeController {
+
+    @FXML
+    private Button OrderButton;
+
+    @FXML
+    private Button ReservationButton;
+
+
+    @FXML
+    void handleOrder(ActionEvent event) throws Exception {
+
+        /*open the sign-in/login window*/
+        Stage login = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("login-overview.fxml"));
+        Scene scene = new Scene(root);
+        login.setScene(scene);
+        login.show();
+    }
+
+    @FXML
+    void handleReservation(ActionEvent event) {
+
+    }
 
 }
 
